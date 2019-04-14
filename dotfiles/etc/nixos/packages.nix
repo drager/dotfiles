1a2bfe02d109e9841d4106b98e35d4157f0eefcb
Jesper Håkansson <jesper@jesperh.se>
2019-04-14 14:39:15 +0200

    Add packages.nix

---
+++ dotfiles/etc/nixos/packages.nix
@@ -0,0 +1,42 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    pkgs.lib.elem (builtins.parseDrvName pkg.name).name
+    [ "firefox-bin" "vscode" "google-chrome" ];
+
+  programs.adb.enable = true;
+  programs.slock.enable = true;
+
+  environment = with pkgs; {
+    systemPackages = [
+      # Tools and utilities.
+      mkpasswd wget curl git xclip theme-vertex file
+      unzip zip ranger keychain
+
+      # Themeing, greeter.
+      numix-gtk-theme numix-icon-theme
+      #gtk #gnome.gnomeicontheme
+
+      # Languages and programming tools.
+      #nodejs-10_x #yarn
+      rustup
+      android-studio
+
+      # Editors.
+      neovim vim vscode
+
+      # Terminal and WM.
+      kitty awesome rofi lightlocker
+      antibody alacritty
+      #polybar 
+
+      # Browsers.
+      firefox
+      chromium google-chrome
+
+      # Others
+      weechat gimp feh
+    ];
+  };
+}
