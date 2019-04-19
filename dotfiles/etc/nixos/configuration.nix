eee3fd7345e755040f6e27c36a0cfa2bebcc4ace
Jesper Håkansson <jesper@jesperh.se>
2019-04-19 16:31:03 +0200

    Enable crypto disk

--- dotfiles/etc/nixos/configuration.nix
+++ dotfiles/etc/nixos/configuration.nix
@@ -13,6 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enableCryptodisk = true;
 
   nixpkgs.config = {
     allowUnfree = true;
